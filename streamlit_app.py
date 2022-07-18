dbfce090ee26404a4ce100c199866e7b2e27175c
codewithrizwan <80236412+codewithrizwan@users.noreply.github.com>
2022-07-18 12:26:18 -0400

    Update streamlit_app.py
    Updated Typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My First Streamlit App')
 streamlit.header('Breakfast Menu')
-streamlit.text(🥣 'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🥗 'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🐔 'Hard-Boiled Free-Range Egg')
-streamlit.text(🥑🍞'Avacado Toast')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
